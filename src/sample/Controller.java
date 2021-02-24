b6c20648e8cfefecfd6efbf6a2eea5bc45f000ab
KeithButler-WIT <20089137@mail.wit.ie>
2021-02-24 16:33:53 +0000

    Test Image Added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -43,7 +43,7 @@ public class Controller {
             File file = fileChooser.showOpenDialog(new Stage());
             imgview.setImage(new Image(file.toURI().toString()));
             image=imgview.getImage();
-            imageReader();
+            getPixelReader();
         });
     }
 
@@ -55,20 +55,23 @@ public class Controller {
     }
 
     public void loadImage(ActionEvent actionEvent) {
-        if((txtfield.getText()==null && txtfield.getText().equals(""))) {
+        if(txtfield.getText()==null || txtfield.getText().equals("")) {
             txtfield.setText("Insert image url here!");
+            //code to make testing easier
+            image = new Image(getClass().getResource("testImage.png").toString(),true);
+            imgview.setImage(image);
+            System.out.println(image.getHeight());
+            getPixelReader();
         } else {
             loadButton.setOnAction(e -> {
-                //InputStream stream = new FileInputStream("/home/keith/Documents/Semester04/DataStructuresAlgorithms2/Worksheet01/testImage.png");
-                // image = new Image(getClass().getResourceAsStream("testImage.png"));
                 image = new Image(txtfield.getText());
                 imgview.setImage(image);
-
-                imageReader();
+                getPixelReader();
             });
         }
     }
-    private void imageReader(){
+
+    private void getPixelReader(){
         // Obtain PixelReader
         pixelReader = image.getPixelReader();
         // Create WritableImage
@@ -97,36 +100,28 @@ public class Controller {
         });
     }
 
-//    opacityLevel.valueProperty().addListener(new ChangeListener<Number>() {
-//        public void changed(ObservableValue<? extends Number> ov,
-//                Number old_val, Number new_val) {
-//            cappuccino.setOpacity(new_val.doubleValue());
-//            opacityValue.setText(String.format("%.2f", new_val));
-//        }
-//    });
-
     public void greyScale(ActionEvent actionEvent){
         // Determine the color of each pixel in a specified row
         for(int readY=0;readY<image.getHeight();readY++) {
             for(int readX=0;readX<image.getWidth();readX++) {
                 Color color=pixelReader.getColor(readX,readY);
-                System.out.println("\nPixel color at coordinates ("
-                        + readX + "," + readY + ") "
-                        + color.toString());
-                System.out.println("R = " + color.getRed());
-                System.out.println("G = " + color.getGreen());
-                System.out.println("B = " + color.getBlue());
-                System.out.println("Opacity = " + color.getOpacity());
-                System.out.println("Saturation = " + color.getSaturation());
+//                System.out.println("\nPixel color at coordinates ("
+//                        + readX + "," + readY + ") "
+//                        + color.toString());
+//                System.out.println("R = " + color.getRed());
+//                System.out.println("G = " + color.getGreen());
+//                System.out.println("B = " + color.getBlue());
+//                System.out.println("Opacity = " + color.getOpacity());
+//                System.out.println("Saturation = " + color.getSaturation());
 
                 // Now write a greyscale color to the PixelWriter.
                 pixelWriter.setColor(readX,readY,color.grayscale());
                 BWimgview.setImage(wImage);
             }
         }
-        System.out.println("Image Width: "+image.getWidth());
-        System.out.println("Image Height: "+image.getHeight());
-        System.out.println("Pixel Format: "+pixelReader.getPixelFormat());
+//        System.out.println("Image Width: "+image.getWidth());
+//        System.out.println("Image Height: "+image.getHeight());
+//        System.out.println("Pixel Format: "+pixelReader.getPixelFormat());
     }
 
     public void removeImage(ActionEvent actionEvent) {
